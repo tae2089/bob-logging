c2dcfe58f8e21f5bc90d3d03d64e8c530092d5ec
tae2089 <tae2089002@gmail.com>
2025-03-08 09:14:12 +0900

    로깅 오류 메시지에서 zap 패키지를 사용하여 에러 정보를 추가함

--- run/main.go
+++ run/main.go
@@ -4,10 +4,11 @@ import (
 	"errors"
 
 	"github.com/tae2089/bob-logging/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
 	logger.Debug("asdasd")
 	logger.Info("123123")
-	logger.Error(errors.New("123123"))
+	logger.Error("error", zap.Error(errors.New("123123")))
 }
